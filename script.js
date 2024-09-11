b368fd372b513b4b11007a15f7b404622bbbf87b
monkeycoinproject <baselev7@gmail.com>
2024-09-11 18:08:50 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 
-    var navBar = document.getElementById("nav-bar");
-    if (navBar.style.display === "none" || navBar.style.display === "") {
+  var navBar = document.getElementById("nav-bar");
+  var tasksButton = document.querySelector(".tasks");
+
+  if (navBar.style.display === "none" || navBar.style.display === "") {
       navBar.style.display = "flex";
-    } else {
+      tasksButton.style.display = "none"; // Hide TASKS button
+  } else {
       navBar.style.display = "none";
-    }
+      tasksButton.style.display = "inline-block"; // Show TASKS button
   }
+}
+function coincollect() {
+   
+  var coinElement = document.getElementById("coin");
+  var currentCoins = parseInt(coinElement.innerText, 10);
+  var newCoins = currentCoins + 1;
+  coinElement.innerText = newCoins;
+  console.log(newCoins);
+}
